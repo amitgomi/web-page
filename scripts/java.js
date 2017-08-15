c9b3f18f26e638646e4d77ae8134cd51697b2686
amitgomi <amitgomi525.ag@gmail.com>
2017-08-15 13:07:48 +0530

    Add files via upload

---
+++ scripts/java.js
@@ -0,0 +1,9 @@
+var a= document.getElementById('mob');
+
+a.addEventListener("click",function(){
+  var b = document.getElementById('side');
+  b.classList.toggle('visible');
+  this.classList.toggle('visibl');
+})
+
+var c=document.querySelector("body");
